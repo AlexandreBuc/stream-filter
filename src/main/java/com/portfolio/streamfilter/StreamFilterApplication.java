b33ba8be4e2699746d876d3c884913175551aa2f
AlexandreBuc <koneochester@gmail.com>
2021-08-26 21:39:48 +0200

    Initiate component

---
+++ src/main/java/com/portfolio/streamfilter/StreamFilterApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.streamfilter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamFilterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StreamFilterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/portfolio/streamfilter/StreamFilterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.streamfilter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamFilterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
